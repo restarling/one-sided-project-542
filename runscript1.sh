62182d0afa70e0f1455a35954cf58e023fd96840
River Sandoval <rivers@wheeler.alliance.unm.edu>
2023-12-05 13:11:33 -0700

    fixed bug in runscript1 and data

--- runscript1.sh
+++ runscript1.sh
@@ -7,6 +7,6 @@ srun -n 1 ./osc 100 >> data.txt;
 srun -n 1 ./osc 150 >> data.txt;
 srun -n 1 ./osc 200 >> data.txt;
 srun -n 1 ./osc 250 >> data.txt;
-srun -n 1 ./osc 250 >> data.txt;
+srun -n 1 ./osc 300 >> data.txt;
 srun -n 1 ./osc 350 >> data.txt;
 srun -n 1 ./osc 400 >> data.txt;
